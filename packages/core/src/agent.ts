7b28e4324a962314e0e0b18a174c33d65ae849f7
LoreviQ <lorevi@protonmail.com>
2025-05-10 00:48:29 +0100

    context refinement

--- packages/core/src/agent.ts
+++ packages/core/src/agent.ts
@@ -1,7 +1,6 @@
+import { Rune, Context } from "./types";
+
 export class Agent {
     private runes: Map<string, Rune> = new Map();
-    private edicts: Map<string, Edict> = new Map();
-    private catalysts: ICatalyst[] = [];
-    private gateway?: IGateway;
-    private context: ContextData = {};
+    private context: Context;
 }
---
+++ packages/core/src/types/index.ts
@@ -0,0 +1,2 @@
+export * from "./rune";
+export * from "./context";
