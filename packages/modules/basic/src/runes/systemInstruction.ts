3cc21b63725dcf6db03fba658ce8a81ab975da78
LoreviQ <lorevi@protonmail.com>
2025-05-10 20:14:55 +0100

    added basic system prefixes

---
+++ packages/modules/basic/src/runes/systemInstruction.ts
@@ -0,0 +1,39 @@
+import { Rune } from "@daedaluskit/core";
+
+export class SystemPrefixRune extends Rune {
+    private systemPrefix: string;
+    constructor(systemPrefix: string) {
+        super(
+            "system_prefix",
+            Number.NEGATIVE_INFINITY,
+            "system",
+            "System Prefix",
+            "The system prefix for the agent.",
+            "0" // Revalidation doesn't matter, data is static anyway
+        );
+        this.systemPrefix = systemPrefix;
+    }
+
+    protected async gather(): Promise<string> {
+        return this.systemPrefix;
+    }
+}
+
+export class SystemSuffixRune extends Rune {
+    private systemSuffix: string;
+    constructor(systemSuffix: string) {
+        super(
+            "system_suffix",
+            Number.POSITIVE_INFINITY,
+            "system",
+            "System Suffix",
+            "The system suffix for the agent.",
+            "0" // Revalidation doesn't matter, data is static anyway
+        );
+        this.systemSuffix = systemSuffix;
+    }
+
+    protected async gather(): Promise<string> {
+        return this.systemSuffix;
+    }
+}
